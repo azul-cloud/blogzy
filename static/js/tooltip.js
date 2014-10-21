48239b8c2758529679704746e08cbb3a854cf7a9
Adam <awwester@gmail.com>
2014-10-21 15:39:51 -0500

    add tooltip for wave button

--- static/js/tooltip.js
+++ static/js/tooltip.js
@@ -3,3 +3,6 @@ This is a separate file because Bootstrap documents that there are performance p
 with including it on all pages. We wil only include this js file on pages that need to
 use the tooltip
 */
+$(function () {
+    $("[data-toggle='tooltip']").tooltip({container: 'body'});
+});
