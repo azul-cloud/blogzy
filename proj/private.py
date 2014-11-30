d3a60482c60722181d85159c0bf293c4e74bc047
Adam <awwester@gmail.com>
2014-11-30 01:22:44 -0500

    add new key for heroku

---
+++ proj/private.py
@@ -0,0 +1,4 @@
+# used to store private data that we don't want public
+
+AWS_S3_ACCESS_KEY_ID = 'AKIAJZECTLPZTVGYRRDQ'     # enter your access key id
+AWS_S3_SECRET_ACCESS_KEY = 'pMbtE6Qn9k0ehr6J8L6/wn6PdgMLfIwfvk+JoPzW' # enter your secret access
