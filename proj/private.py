ad013bc477f3ad752529925facc794eec944dcf5
Adam <awwester@gmail.com>
2014-11-30 01:36:38 -0500

    delete private

--- proj/private.py
+++
@@ -1,4 +0,0 @@
-# used to store private data that we don't want public
-
-AWS_S3_ACCESS_KEY_ID = 'AKIAJZECTLPZTVGYRRDQ'     # enter your access key id
-AWS_S3_SECRET_ACCESS_KEY = 'pMbtE6Qn9k0ehr6J8L6/wn6PdgMLfIwfvk+JoPzW' # enter your secret access
