cd62a6afc3f1675288c9d95675d604d0fcbc7720
Adam Wester <awwester@gmail.com>
2014-12-21 10:48:40 -0500

    allow all hosts

--- settings/production.py
+++ settings/production.py
@@ -2,8 +2,6 @@ from .staging import *
 
 
 # Allow all host headers
-ALLOWED_HOSTS = ['http://www.travelblogwave.com/', 
-    'http://tbwv.herokuapp.com/']
 
 MAILGUN_SERVER_NAME = 'travelblogwave.com'
 
