79495b42409a497b1dc4c9cf105c834151770cc8
Adam Wester <awwester@gmail.com>
2014-12-21 10:42:11 -0500

    remove prod storage bucket

--- settings/production.py
+++ settings/production.py
@@ -9,4 +9,4 @@ ALLOWED_HOSTS = ['http://travelblogwave.com/']
 # DEBUG = False
 # TEMPLATE_DEBUG = False
 
- AWS_STORAGE_BUCKET_NAME = 'travelblogwave.media'
+ # AWS_STORAGE_BUCKET_NAME = 'travelblogwave.media'
