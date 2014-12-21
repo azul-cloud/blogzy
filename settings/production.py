074ece3ece2e26bb9d0a25b13f6faa2d8a8ae8fb
Adam Wester <awwester@gmail.com>
2014-12-21 10:51:09 -0500

    static files dirs prod

--- settings/production.py
+++ settings/production.py
@@ -8,4 +8,11 @@ MAILGUN_SERVER_NAME = 'travelblogwave.com'
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+if DEBUG:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
  # AWS_STORAGE_BUCKET_NAME = 'travelblogwave.media'
