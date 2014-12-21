f610ab8486aa15cabe1769c436f07dd5af0b0f86
Adam Wester <awwester@gmail.com>
2014-12-21 10:33:58 -0500

    add allowed hosts to prod

--- settings/production.py
+++ settings/production.py
@@ -4,9 +4,9 @@ from .staging import *
 # Allow all host headers
 ALLOWED_HOSTS = ['http://travelblogwave.com/']
 
-# MAILGUN_SERVER_NAME = 'travelblogwave.com'
+MAILGUN_SERVER_NAME = 'travelblogwave.com'
 
 # DEBUG = False
 # TEMPLATE_DEBUG = False
 
-# AWS_STORAGE_BUCKET_NAME = 'travelblogwave.media'
+ AWS_STORAGE_BUCKET_NAME = 'travelblogwave.media'
