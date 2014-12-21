2a0cedc09879af2eb672f03ba6de95a85cfd26ea
Adam Wester <awwester@gmail.com>
2014-12-21 10:49:24 -0500

    allow all hosts

--- settings/staging.py
+++ settings/staging.py
@@ -15,7 +15,7 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['http://tbwvtest.herokuapp.com/']
+# ALLOWED_HOSTS = ['http://tbwvtest.herokuapp.com/']
 
 MAILGUN_ACCESS_KEY = os.environ['MAILGUN_ACCESS_KEY']
 MAILGUN_SERVER_NAME = 'sandboxbea330ddebf24842829144f24a61eaa1.mailgun.org'
