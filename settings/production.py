760ef688d43dc3357c5c9d62521d99fcc396c7fb
Adam Wester <awwester@gmail.com>
2014-12-21 10:32:23 -0500

    add allowed hosts to prod

--- settings/production.py
+++ settings/production.py
@@ -2,7 +2,7 @@ from .staging import *
 
 
 # Allow all host headers
-# ALLOWED_HOSTS = ['http://travelblogwave.com/']
+ALLOWED_HOSTS = ['http://travelblogwave.com/']
 
 # MAILGUN_SERVER_NAME = 'travelblogwave.com'
 
