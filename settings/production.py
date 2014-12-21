a9621bb52e4ce4b592cad16a1cba28568be2b41d
Adam Wester <awwester@gmail.com>
2014-12-21 10:30:59 -0500

    set prod to staging

--- settings/production.py
+++ settings/production.py
@@ -2,11 +2,11 @@ from .staging import *
 
 
 # Allow all host headers
-ALLOWED_HOSTS = ['http://travelblogwave.com/']
+# ALLOWED_HOSTS = ['http://travelblogwave.com/']
 
-MAILGUN_SERVER_NAME = 'travelblogwave.com'
+# MAILGUN_SERVER_NAME = 'travelblogwave.com'
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+# DEBUG = False
+# TEMPLATE_DEBUG = False
 
-AWS_STORAGE_BUCKET_NAME = 'travelblogwave.media'
+# AWS_STORAGE_BUCKET_NAME = 'travelblogwave.media'
