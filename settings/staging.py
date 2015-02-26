7e7f42c42e969fe830759269284e253e627d12a7
Adam Wester <awwester@gmail.com>
2015-02-26 15:06:45 -0300

    s3 for static

--- settings/staging.py
+++ settings/staging.py
@@ -26,3 +26,6 @@ DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+STATIC_URL = 'http://s3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME + '/'
