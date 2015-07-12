499ea10f0284aa7cee4e2876c58061dc4e62ac0c
Adam Wester <awwester@gmail.com>
2015-07-11 22:45:58 -0500

    staging settings

--- settings/staging.py
+++ settings/staging.py
@@ -1,13 +1,11 @@
+import dj_database_url
+
 from .base import *
 
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-import dj_database_url
 DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
@@ -27,6 +25,3 @@ MAILGUN_ACCESS_KEY = os.environ['MAILGUN_ACCESS_KEY']
 MAILGUN_SERVER_NAME = 'sandboxbea330ddebf24842829144f24a61eaa1.mailgun.org'
 
 WEB_ROOT_URL = 'http://test.travelblogwave.com'
-
-DATABASES = {}
-DATABASES['default'] =  dj_database_url.config()
