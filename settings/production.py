2f5676a2c1aa285f6c1f27a6443fd8c29fc59f91
Adam Wester <awwester@gmail.com>
2015-07-11 22:37:12 -0500

    change prod settings

--- settings/production.py
+++ settings/production.py
@@ -1,18 +1,6 @@
 from .staging import *
 
 
-# Allow all host headers
-
-DEBUG = False
-TEMPLATE_DEBUG = False
-
-if DEBUG:
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 AWS_STORAGE_BUCKET_NAME = 'travelblogwave.media'
 
 MAILGUN_SERVER_NAME = 'travelblogwave.com'
